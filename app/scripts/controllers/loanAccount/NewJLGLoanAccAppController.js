98ae2f11fc1495be23d59030fbe81546b2bb8495
Alberto <jose.hernandez@fintecheando.mx>
2019-12-30 18:12:45 -0600

    Actualizacion pantalla de credito JLG

--- app/scripts/controllers/loanAccount/NewJLGLoanAccAppController.js
+++ app/scripts/controllers/loanAccount/NewJLGLoanAccAppController.js
@@ -2,7 +2,7 @@
     mifosX.controllers = _.extend(module, {
         NewJLGLoanAccAppController: function (scope, rootScope, routeParams, resourceFactory, location, dateFilter) {
 
-            scope.response = {success:[],failed:[]};
+            scope.response = { success: [], failed: [] };
             scope.group = {};
             scope.group.selectedclients = [];
             scope.group.id = routeParams.groupId;
@@ -13,7 +13,10 @@
             scope.loanApplicationCommonData = {};  // user set common data for all the loan applications
             scope.loanApplicationCommonData.submittedOnDate = new Date();
             scope.loanApplicationCommonData.expectedDisbursementDate = new Date();
+            scope.loanApplicationCommonData.repaymentsStartingFromDate = new Date();
             scope.loanApplicationCommonData.syncDisbursementWithMeeting = true;
+            scope.channelOptions = [];
+            scope.paymentFor1000Options = [];
             scope.datatables = [];
             scope.noOfTabs = 1;
             scope.step = '-';
@@ -31,14 +34,18 @@
 
             // Fetch loan products for initital product drop-down
             resourceFactory.loanResource.get(scope.inparams, function (data) {
-                scope.products = data.productOptions;
+                scope.products = [];
+                for (var i in data.productOptions) {
+                    if (data.productOptions[i].shortName.startsWith("GR")) {
+                        scope.products.push(data.productOptions[i]);
+                    }
+                }
                 scope.datatables = data.datatables;
                 if (data.group) {
                     scope.group.name = data.group.name;
                 }
                 scope.handleDatatables(scope.datatables);
             });
-
 
             scope.loanProductChange = function (loanProductId) {
                 _.isUndefined(scope.datatables) ? scope.tempDataTables = [] : scope.tempDataTables = scope.datatables;
@@ -47,13 +54,14 @@
 
                     scope.productDetails = data.product;
                     console.log('scope', scope.productDetails);
-                    scope.group.clients = data.group.clientMembers.map(function(client) {
+                    scope.group.clients = data.group.clientMembers.map(function (client) {
                         client.principal = data.product.principal;
-                        client.charges = data.product.charges.map(function(charge){
-                            charge.isDeleted = false; 
-                            return _.clone(charge);});
+                        client.charges = data.product.charges.map(function (charge) {
+                            charge.isDeleted = false;
+                            return _.clone(charge);
+                        });
                         // return was returing the reference, instead the value, so added _.clone
-                        return client; 
+                        return client;
                     });
 
                     scope.loanOfficers = data.loanOfficerOptions;
@@ -67,6 +75,23 @@
                 });
             };
 
+            resourceFactory.codeResources.getAllCodes({}, function (data) {
+                scope.codes = data;
+                scope.channelOptions = [];
+                scope.paymentFor1000Options = [];
+                for (var i in data) {
+                    if (data[i].name == "PAYMENT_CHANNEL") {
+                        resourceFactory.codeValueResource.getAllCodeValues({codeId: data[i].id}, function (data) {
+                            scope.channelOptions = data;
+                        });
+                    } else if (data[i].name == "PAYMENT_FOR1000") {
+                        resourceFactory.codeValueResource.getAllCodeValues({codeId: data[i].id}, function (data) {
+                            scope.paymentFor1000Options = data;
+                        });
+                    }  
+                }
+            });
+
             scope.handleDatatables = function (datatables) {
                 if (!_.isUndefined(datatables) && datatables.length > 0) {
                     scope.formData.datatables = [];
@@ -76,13 +101,13 @@
                         scope.updateColumnHeaders(datatable.columnHeaderData);
                         angular.forEach(datatable.columnHeaderData, function (colHeader, i) {
                             if (_.isEmpty(scope.formDat.datatables[index])) {
-                                scope.formDat.datatables[index] = {data: {}};
+                                scope.formDat.datatables[index] = { data: {} };
                             }
 
                             if (_.isEmpty(scope.formData.datatables[index])) {
                                 scope.formData.datatables[index] = {
                                     registeredTableName: datatable.registeredTableName,
-                                    data: {locale: scope.optlang.code}
+                                    data: { locale: scope.optlang.code }
                                 };
                             }
 
@@ -94,7 +119,7 @@
                 }
             };
 
-            scope.updateColumnHeaders = function(columnHeaderData) {
+            scope.updateColumnHeaders = function (columnHeaderData) {
                 var colName = columnHeaderData[0].columnName;
                 if (colName == 'id') {
                     columnHeaderData.splice(0, 1);
@@ -126,35 +151,33 @@
             };
 
             scope.toggleCharge = function (clientIndex, chargeIndex) {
-
                 // scope.group.clients[clientIndex].charges.splice(chargeIndex,1);
-                if(scope.group.clients[clientIndex].charges[chargeIndex].isDeleted){
+                if (scope.group.clients[clientIndex].charges[chargeIndex].isDeleted) {
                     scope.group.clients[clientIndex].charges[chargeIndex].isDeleted = false;
                 }
-                else{
+                else {
                     scope.group.clients[clientIndex].charges[chargeIndex].isDeleted = true;
                 }
-
             };
 
-            scope.checkerInboxAllCheckBoxesClicked = function() {
+            scope.checkerInboxAllCheckBoxesClicked = function () {
                 scope.isAllClientSelected = !scope.isAllClientSelected;
-                if(!angular.isUndefined(scope.group.clients)) {
+                if (!angular.isUndefined(scope.group.clients)) {
                     for (var i in scope.group.clients) {
                         scope.group.clients[i].isSelected = scope.isAllClientSelected;
                     }
                 }
             }
 
-            scope.checkerInboxAllCheckBoxesMet = function() {
-                if(!angular.isUndefined(scope.group.clients)) {
+            scope.checkerInboxAllCheckBoxesMet = function () {
+                if (!angular.isUndefined(scope.group.clients)) {
                     var count = 0;
                     for (var i in scope.group.clients) {
-                        if(scope.group.clients[i].isSelected){
+                        if (scope.group.clients[i].isSelected) {
                             count++;
                         }
                     }
-                    scope.isAllClientSelected = (scope.group.clients.length==count);
+                    scope.isAllClientSelected = (scope.group.clients.length == count);
                     return scope.isAllClientSelected;
                 }
             }
@@ -185,26 +208,39 @@
                     delete scope.formData.datatables;
                 }
 
+                // Interest Rate Per Period using payment for each 1,000
+                var interestRatePerPeriod = 0;
+                for (var i in scope.paymentFor1000Options) {
+                    if (scope.paymentFor1000Id == scope.paymentFor1000Options[i].id) {
+                        interestRatePerPeriod = scope.paymentFor1000Options[i].description;
+                        break;
+                    }
+                }
+
                 this.batchRequests = [];
                 for (var i in scope.group.clients) {
-                    if( scope.group.clients[i].isSelected ){
-
+                    if (scope.group.clients[i].isSelected) {
                         var loanApplication = {};
-
                         loanApplication.locale = scope.optlang.code;
-                        loanApplication.dateFormat =  scope.df;
+                        loanApplication.dateFormat = scope.df;
                         loanApplication.groupId = scope.group.id;
                         loanApplication.clientId = scope.group.clients[i].id;
-                        if(scope.caledars){
+                        if (scope.caledars) {
                             loanApplication.calendarId = scope.caledars[0].id;
                         }
                         loanApplication.loanType = 'jlg';
                         loanApplication.productId = scope.productDetails.id;
                         loanApplication.fundId = scope.loanApplicationCommonData.fundId;
+                        if (interestRatePerPeriod > 0) {
+                            loanApplication.interestRatePerPeriod = interestRatePerPeriod;
+                        } else {
+                            loanApplication.interestRatePerPeriod = scope.productDetails.interestRatePerPeriod;
+                        }
+                        // loanApplication.channelId = scope.loanApplicationCommonData.channelId;
+                        loanApplication.repaymentsStartingFromDate = dateFilter(scope.loanApplicationCommonData.firstrepaymenton, scope.dateFormat);
                         loanApplication.numberOfRepayments = scope.productDetails.numberOfRepayments;
                         loanApplication.repaymentEvery = scope.productDetails.repaymentEvery;
                         loanApplication.repaymentFrequencyType = scope.productDetails.repaymentFrequencyType.id;
-                        loanApplication.interestRatePerPeriod = scope.productDetails.interestRatePerPeriod;
                         loanApplication.amortizationType = scope.productDetails.amortizationType.id;
                         loanApplication.interestType = scope.productDetails.interestType.id;
                         loanApplication.interestCalculationPeriodType = scope.productDetails.interestCalculationPeriodType.id;
@@ -219,15 +255,12 @@
                         loanApplication.loanOfficerId = scope.loanApplicationCommonData.loanOfficerId;
                         loanApplication.principal = scope.group.clients[i].principal;
                         loanApplication.expectedDisbursementDate = dateFilter(scope.loanApplicationCommonData.expectedDisbursementDate, scope.df);
-                        loanApplication.submittedOnDate =  dateFilter(scope.loanApplicationCommonData.submittedOnDate, scope.df);
+                        loanApplication.submittedOnDate = dateFilter(scope.loanApplicationCommonData.submittedOnDate, scope.df);
                         loanApplication.syncDisbursementWithMeeting = scope.loanApplicationCommonData.syncDisbursementWithMeeting;
 
-
                         loanApplication.charges = [];
-
                         for (var j in scope.group.clients[i].charges) {
-
-                            if(!scope.group.clients[i].charges[j].isDeleted && scope.group.clients[i].charges[j].chargeTimeType.code !="chargeTimeType.overdueInstallment"){
+                            if (!scope.group.clients[i].charges[j].isDeleted && scope.group.clients[i].charges[j].chargeTimeType.code != "chargeTimeType.overdueInstallment") {
                                 var charge = {};
                                 charge.amount = scope.group.clients[i].charges[j].amount;
                                 charge.chargeId = scope.group.clients[i].charges[j].id;
@@ -239,40 +272,34 @@
                             loanApplication.datatables = scope.formData.datatables;
                         }
 
-                        this.batchRequests.push({requestId: i, relativeUrl: "loans",
-                            method: "POST", body: JSON.stringify(loanApplication)});
-
+                        this.batchRequests.push({
+                            requestId: i, relativeUrl: "loans",
+                            method: "POST", body: JSON.stringify(loanApplication)
+                        });
                     }
-
                 }
 
                 resourceFactory.batchResource.post(this.batchRequests, function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        if (data[i].statusCode == 200)
+                            scope.response.success.push(data[i]);
+                        else
+                            scope.response.failed.push(data[i]);
 
-                        for (var i = 0; i < data.length; i++) {
-                                if(data[i].statusCode == 200 ) 
-                                    scope.response.success.push(data[i]);
-                                else
-                                    scope.response.failed.push(data[i]);
+                    }
 
-                            }   
-
-                        if(scope.response.failed.length === 0 ){
-                            location.path('/viewgroup/' + scope.group.id);    
-                        }
-
+                    if (scope.response.failed.length === 0) {
+                        location.path('/viewgroup/' + scope.group.id);
+                    }
                 });
-
-                
-            }; 
+            };
 
             /* Cancel button action */
             scope.cancel = function () {
                 if (scope.group.id) {
                     location.path('/viewgroup/' + scope.group.id);
                 }
-            };             
-
-
+            };
         } // End of NewJLGLoanAccAppController
 
     });
