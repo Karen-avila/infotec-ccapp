d860a6a47b66847513a29f16a68c6d02feb3b20f
INFOTEC <60331640+InfotecMexico@users.noreply.github.com>
2020-04-08 01:33:52 -0500

    Cambios

--- gems.rb
+++ gems.rb
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
-gem 'compass', '>=1.0.3'
-gem 'sass', '3.5'
+gem 'compass', '>1'
+gem 'sass', '>3'
 gem 'sass-css-importer', '~> 1.0.0.beta.0'
-gem 'scss_lint', '~> 0.59.0'
-gem 'font-awesome-sass', '~> 5.12.0'
+gem 'scss_lint', '> 0'
+gem 'font-awesome-sass', '> 5'
