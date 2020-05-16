06ad13fb109df9b3496fcbc8bb3b6311b17b4895
gustavao <elg.gespindola@gmail.com>
2020-05-15 19:29:49 -0500

    createOffice

--- app/scripts/controllers/organization/CreateOfficeController.js
+++ app/scripts/controllers/organization/CreateOfficeController.js
@@ -1,26 +1,54 @@
-(function (module) {
+(function(module) {
     mifosX.controllers = _.extend(module, {
-        CreateOfficeController: function (scope, resourceFactory, location, dateFilter) {
+        CreateOfficeController: function(scope, resourceFactory, location, dateFilter) {
             scope.offices = [];
             scope.first = {};
             scope.first.date = new Date();
             scope.restrictDate = new Date();
-            resourceFactory.officeResource.getAllOffices(function (data) {
+            //--------
+            scope.addressTypes = [];
+            scope.countryOptions = [];
+            scope.stateOptions = [];
+            scope.allMunicipalityOptions = [];
+            scope.municipalityOptions = [];
+            scope.addressTypeId = {};
+            entityname = "ADDRESS";
+            //--------
+            resourceFactory.officeResource.getAllOffices(function(data) {
                 scope.offices = data;
                 scope.formData = {
                     parentId: scope.offices[0].id
                 }
             });
 
-             scope.minDat = function() {
-                 for(var i=0;i<scope.offices.length;i++) {
-                     if ((scope.offices[i].id) === (scope.formData.parentId)) {
-                         return scope.offices[i].openingDate;
-                     }
-                 }
-                };
+            //----------
+            resourceFactory.clientTemplateResource.get(function(data) {
 
-            scope.submit = function () {
+                scope.addressTypes = data.address[0].addressTypeIdOptions;
+                scope.countryOptions = data.address[0].countryIdOptions;
+                scope.stateOptions = data.address[0].stateProvinceIdOptions;
+                scope.municipalityOptions = data.address[0].municipalityIdOptions;
+                scope.allMunicipalityOptions = data.address[0].municipalityIdOptions;
+                resourceFactory.addressFieldConfiguration.get({ entity: entityname }, function(data) {
+                    for (var i = 0; i < data.length; i++) {
+                        data[i].field = 'scope.' + data[i].field;
+                        eval(data[i].field + "=" + data[i].is_enabled);
+                    }
+                })
+
+            });
+            //---------
+
+            scope.minDat = function() {
+                for (var i = 0; i < scope.offices.length; i++) {
+                    if ((scope.offices[i].id) === (scope.formData.parentId)) {
+                        return scope.offices[i].openingDate;
+                    }
+                }
+            };
+
+            scope.submit = function() {
+                console.log("Se envia", this.formData);
                 this.formData.locale = scope.optlang.code;
                 var reqDate = dateFilter(scope.first.date, scope.df);
                 this.formData.dateFormat = scope.df;
@@ -28,13 +56,13 @@
                 this.formData.name = this.formData.name.toUpperCase();
                 this.formData.city = this.formData.city.padStart(3, "0");
                 this.formData.branch = this.formData.branch.padStart(6, "0");
-                resourceFactory.officeResource.save(this.formData, function (data) {
+                resourceFactory.officeResource.save(this.formData, function(data) {
                     location.path('/viewoffice/' + data.resourceId);
                 });
             };
         }
     });
-    mifosX.ng.application.controller('CreateOfficeController', ['$scope', 'ResourceFactory', '$location', 'dateFilter', mifosX.controllers.CreateOfficeController]).run(function ($log) {
+    mifosX.ng.application.controller('CreateOfficeController', ['$scope', 'ResourceFactory', '$location', 'dateFilter', mifosX.controllers.CreateOfficeController]).run(function($log) {
         $log.info("CreateOfficeController initialized");
     });
 }(mifosX.controllers || {}));
