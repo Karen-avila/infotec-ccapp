7d08c8e7cdffcfe4ad0931128a2cf1853dc145ed
charlieharo <carlos.haro@fintecheando.mx>
2020-06-17 17:17:20 -0500

    Realizo traducciones, y correción de ortografía.

--- app/scripts/controllers/product/CreateLoanProductController.js
+++ app/scripts/controllers/product/CreateLoanProductController.js
@@ -8,7 +8,7 @@
             scope.accountingOptions = ['None', 'Cash', 'Accrual(Periodic)', 'Accrual(Upfront)'];
             scope.floatingrateoptions = [];
             scope.loanProductConfigurableAttributes = [];
-            scope.showOrHideValue = "show";
+            scope.showOrHideValue = "Mostrar";
             scope.configureFundOptions = [];
             scope.specificIncomeAccountMapping = [];
             scope.penaltySpecificIncomeaccounts = [];
@@ -156,12 +156,12 @@
             //advanced accounting rule
             scope.showOrHide = function (showOrHideValue) {
 
-                if (showOrHideValue == "show") {
-                    scope.showOrHideValue = 'hide';
+                if (showOrHideValue == "Mostrar") {
+                    scope.showOrHideValue = 'Ocultar';
                 }
 
-                if (showOrHideValue == "hide") {
-                    scope.showOrHideValue = 'show';
+                if (showOrHideValue == "Ocultar") {
+                    scope.showOrHideValue = 'Mostrar';
                 }
             };
 
