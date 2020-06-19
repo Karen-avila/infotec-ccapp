b3297e0d26715ccc029e0d0b8251731cc8c951a5
karen <ana.avila@infotec.mx>
2020-06-18 22:57:12 -0500

    Se agregan Librerias  para el mapa en viewOffice

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,7 +200,8 @@ module.exports = function(grunt) {
                 '!jasmine/**', '!requirejs/**/**', 'requirejs/require.js', '!underscore/**', 'angular-bootstrap/*.css', 'ng-scrollbar/**/*.css', 
                 'angular-loading-bar/build/*.min.*', 'fontawesome/css/*.min.css', 'angular-utils-pagination/dirPagination.tpl.html','moment/min/*.min.js',
                 'pdfjs/src/pdf*.js', 'pdfjs/src/shared/*.js', 'pdfjs/src/display/*.js', 'angular-material-data-table/dist/*.js', 'angular-material-data-table/dist/*.css',
-                "md-steppers/dist/*.min.*"
+                "md-steppers/dist/*.min.*",'leaflet/dist/leaflet.js','leaflet/dist/leaflet.css', 'angular-leaflet-directive/dist/*.js'
+                
             ]
             }
             ]
