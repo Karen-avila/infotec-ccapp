8d238765195fd0db0020373b8ef7e712e9ca6c18
Miguel Albores <miguelalboresc@gmail.com>
2020-10-28 03:31:49 -0600

    Se agrega el correo al momento de activar el BPI

--- app/scripts/controllers/client/ViewClientController.js
+++ app/scripts/controllers/client/ViewClientController.js
@@ -575,7 +575,7 @@
 
             var ClientActiveBPICtrl = function ($scope, $uibModalInstance) {
                 $scope.activeBPI = function () {
-                    resourceFactory.clientResource.save({ clientId: scope.clientId, command : "activateBPIService" }, {}, function (data) {
+                    resourceFactory.clientResource.save({ clientId: scope.clientId, command : "activateBPIService" }, this.formData, function (data) {
                         $uibModalInstance.close('activeBPI');
                         location.path('/clients');
                     });
