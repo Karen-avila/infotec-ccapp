e83aff5b84a4aa6b88521a83ef58c52e867f2014
karen1015A <ana.avila@infotec.mx>
2020-11-13 13:27:35 -0600

    denominacion en servicespay y reporte de saldos

--- app/scripts/controllers/servicespay/ServicesPayController.js
+++ app/scripts/controllers/servicespay/ServicesPayController.js
@@ -1,9 +1,17 @@
 (function(module) {
     mifosX.controllers = _.extend(module, {
-        ServicesPayController: function(scope, location, resourceFactory) {
+        ServicesPayController: function(scope, location, resourceFactory, translate) {
 
-            scope.hola = "hello";
-
+            scope.template=[];
+            scope.sumaDenominations=0;
+            scope.formData={};
+            scope.formData.amount=0;
+            scope.first = {
+                date: new Date()
+            };
+            scope.restrictDate = new Date();
+            scope.formData.denominations = [{denominacion:"1000",cantidad:0,total:0} ,{denominacion:"500",cantidad:0,total:0},
+            {denominacion:"200",cantidad:0,total:0},{denominacion:"100",cantidad:0,total:0},{denominacion:"50",cantidad:0,total:0}, {denominacion:"20",cantidad:0,total:0}];  
             resourceFactory.paymentTypeResource.getAll(function (data) {
                 scope.paymentTypes = data;
             });
@@ -12,8 +20,29 @@
                 scope.paymentthirdServices = data;
             });
 
-        }
-    });
+            scope.Total= function(denomination,index)
+            {
+              scope.formData.denominations[index].total=  (scope.formData.denominations[index].cantidad * denomination);
+              scope.sumaDenominations=0;
+              scope.formData.denominations.forEach(element => {
+                scope.sumaDenominations=scope.sumaDenominations + element.total;
+              });
+            };
+          
+            scope.numeros = function(e){
+           
+              let key = window.Event ? e.which : e.keyCode
+              console.log("entro",key);
+             if (key >= 48 && key <= 57)
+             {
+    
+             }
+             else{
+              e.preventDefault();
+             }
+            };
+          },
+        });
     mifosX.ng.application.controller('ServicesPayController', ['$scope', '$location', 'ResourceFactory', mifosX.controllers.ServicesPayController]).run(function($log) {
         $log.info("ServicesPayController initialized");
     });
